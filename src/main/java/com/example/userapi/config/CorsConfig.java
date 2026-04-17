d6bdc3445d03bbcbe993dbbe9740463898b4b03e
flexpartha <partha.chakra2@gmail.com>
2026-04-17 15:19:20 +0530

    cors updated

--- src/main/java/com/example/userapi/config/CorsConfig.java
+++ src/main/java/com/example/userapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // your frontend URL
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://frontend-ij67.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
